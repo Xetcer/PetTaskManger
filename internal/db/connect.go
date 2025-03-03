9de5b86c36dc386f5c989e18167c924dbe0a07a9
Xetcer <ferdinand_tema@mail.ru>
2025-03-03 09:13:41 +0500

    move create DB to migration.go

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -33,14 +33,3 @@ func ConnectDB(stg PSQLConnectStg) (*sql.DB, error) {
 
 	return db, nil
 }
-
-// createDB созадем нужную БД, вернем ее или ошибку
-func CreateDB(db *sql.DB, name string) error {
-	if db != nil {
-		createStr := fmt.Sprintf("CREATE DATABASE %s;", name)
-		_, err := db.Exec(createStr)
-		return err
-	} else {
-		return fmt.Errorf("Empty db pointer")
-	}
-}
